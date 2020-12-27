a97bed2235d8687e0aa26ed1311cb20a01f91377
Jean Cabral <jeancabral@gmail.com>
2020-12-27 06:18:44 -0300

    chore: run uvicorn from app

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from fastapi import FastAPI
+import uvicorn
+from app.main import build_api
 
-app = FastAPI()
+app = build_api()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+if __name__ == '__main__':
+    uvicorn.run(app, port=5000, log_level="info")
