4beaed97add48c831be6f154146926d509ddaae1
Jean Cabral <jeancabral@gmail.com>
2021-01-10 22:06:37 -0300

    chore: styling

--- app/todo/model.py
+++ app/todo/model.py
@@ -2,31 +2,33 @@ from pydantic import BaseModel, Field
 from typing import Optional
 from enum import Enum
 
+
 class StatusOptions(str, Enum):
-  not_completed = "Não concluído"
-  doing = "Fazendo"
-  done = "Feito"
+    not_completed = "Não concluído"
+    doing = "Fazendo"
+    done = "Feito"
+
 
 class TodoItem(BaseModel):
-  title: str = Field(...)
-  description: Optional[str]
-  status: Optional[StatusOptions]
+    title: str = Field(...)
+    description: Optional[str]
+    status: Optional[StatusOptions]
 
-  class Config:
-      schema_extra = {
-          "example": {
-              "title": "Some Title",
-              "description": "Describe your todo",
-              "status": "Feito",
-          }
-      }
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "Some Title",
+                "description": "Describe your todo",
+                "status": "Feito",
+            }
+        }
 
 
 class TodoItemResponse(BaseModel):
-  id: int
-  title: str
-  description: str = None
-  status: Optional[StatusOptions]
+    id: int
+    title: str
+    description: str = None
+    status: Optional[StatusOptions]
 
 
 def ResponseModel(data, message):
